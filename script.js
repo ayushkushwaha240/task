6c60f51fab473aca887a2575a88df9c8d64f5b8d
Ayush Kushwaha <148432773+ayushkushwaha240@users.noreply.github.com>
2024-10-12 18:48:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+async function sendQuery() {
+    const query = document.getElementById("queryInput").value;
+
+    try {
+        const response = await fetch('http://localhost:8000/find_best_match/', { // Update with your backend URL
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ query: query }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        
+        // Display results
+        if (data.similar_sentence && data.best_match) {
+            document.getElementById("resultOutput").innerText = `Similar Sentence: ${data.similar_sentence}\nBest Match: ${data.best_match}`;
+        } else {
+            document.getElementById("resultOutput").innerText = "Stock not found";
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        document.getElementById("resultOutput").innerText = 'Error: ' + error.message;
+    }
+}
